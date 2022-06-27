7de22624f4e45096ce6fc387d300ff6c07a98403
Nick <nikita.nikolenko@gmail.com>
2022-06-27 16:10:32 +0100

    Added Procfile

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
 
